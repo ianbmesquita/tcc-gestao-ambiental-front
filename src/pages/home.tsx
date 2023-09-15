5e2993e725a9e6b846d8fcb7085fd72beafe0795
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-15 14:52:03 -0300

    feat: Criando a estrutura inicial do projeto usando Next.JS

---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div>
+            Página inicial do SIGAM
+        </div>
+    )
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div>
+            Página de autenticação do usuário
+        </div>
+    )
+}
