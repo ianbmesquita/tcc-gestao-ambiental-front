67b341bde00dec2f7e47258afde2050e3f5e674c
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-17 02:20:45 -0300

    feat: Criando tela de cadastro de usuários e seus componentes.

--- src/pages/cadastro-usuario.tsx
+++ src/pages/cadastro-usuario.tsx
@@ -1,9 +1,9 @@
 import styles from '../styles/CadastroUsuarios.module.css'
 
 export default function CadastroUsuario() {
-    const inputClass = "border border-gray-300 rounded-md text-gray-600 h-10 pl-5 pr-10 bg-white focus:outline-none mb-3 sm:w-1/2 lg:w-11/12"
-    const selectBoxClass = "border border-gray-300 rounded-md text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none mb-3 sm:w-1/2 lg:w-11/12"
-    const buttonClass = "bg-emerald-950 transition duration-200 text-white px-4 py-2 rounded hover:bg-emerald-700 font-semibold mt-5 sm:w-1/2 lg:w-1/12 ml-auto"
+    const inputClass = "border border-gray-300 rounded-md text-gray-600 h-10 pl-5 pr-10 bg-white focus:outline-none mb-3 w-full lg:w-11/12"
+    const selectBoxClass = "border border-gray-300 rounded-md text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none mb-3 w-full lg:w-11/12"
+    const buttonClass = "bg-emerald-950 transition duration-200 text-white px-4 py-2 rounded hover:bg-emerald-700 font-semibold mt-5 w-full lg:w-1/12 ml-auto"
 
     async function handleNewUser(event: React.FormEvent) {
         event.preventDefault();
@@ -13,23 +13,27 @@ export default function CadastroUsuario() {
     
     return (
         <div>
-            <div className="text-3xl font-bold text-gray-800 mb-10 mt-10">
+            <div className="text-3xl font-bold text-gray-800 mb-10 mt-10 text-center lg:text-left">
                 <span className={styles.text}>Cadastro de Usuário</span> 
             </div>
             <div>
-                <form className="grid grid-cols-2 gap-4" onSubmit={handleNewUser}>
-                    <div className="col-span-1">
+                <form className="grid grid-cols-1 md:grid-cols-2 gap-4 p-3" onSubmit={handleNewUser}>
+                    <div>
                         <label htmlFor="name" className="block text-gray-700 font-semibold">Nome:</label>
                         <input className={inputClass} type="text" name="name" placeholder="Nome completo" />
+                    </div>
 
+                    <div>
+                        <label htmlFor="login" className="block text-gray-700 font-semibold">Login:</label>
+                        <input className={inputClass} type="text" name="login" placeholder="E-mail" />
+                    </div>
+
+                    <div>
                         <label htmlFor="password" className="block text-gray-700 font-semibold">Senha:</label>
                         <input className={inputClass} type="password" name="password" placeholder="Senha" />
                     </div>
 
-                    <div className="col-span-1">
-                        <label htmlFor="login" className="block text-gray-700 font-semibold">Login:</label>
-                        <input className={inputClass} type="text" name="login" placeholder="E-mail" />
-
+                    <div>
                         <label htmlFor="role" className="block text-gray-700 font-semibold">Categoria:</label>
                         <select className={selectBoxClass} name="role">
                             <option>Selecione</option>
@@ -40,7 +44,7 @@ export default function CadastroUsuario() {
                         </select>
                     </div>
 
-                    <div className="col-span-2">
+                    <div className="md:col-span-2">
                         <button className={buttonClass}>Cadastrar</button>
                     </div>
                 </form>
